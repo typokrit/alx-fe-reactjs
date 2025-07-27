24d4133bd7530cac148b15f9d1d6b8ad841efbce
typokrit <abenianemaw731@gmail.com>
2025-07-27 17:15:47 +0300

    add new react

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -19,6 +20,7 @@ function App() {
       <div>
         <Header />
         <MainContent />
+        <Counter />
         <Footer />
       </div>
       <div>
---
+++ alx-react-app-new/src/components/Counter.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: "center", margin: "20px" }}>
+      <p style={{ fontSize: "24px" }}>Current Count: {count}</p>
+      <button
+        onClick={() => setCount(count + 1)}
+        style={{ marginRight: "10px" }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => setCount(count - 1)}
+        style={{ marginRight: "10px" }}
+      >
+        Decrement
+      </button>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </div>
+  );
+}
+
+export default Counter;
