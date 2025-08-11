1819857636738be87a653c39d797d31da3c8ae61
typokrit <abenianemaw731@gmail.com>
2025-08-11 15:00:44 +0300

    fix:added grid-cols-1 for responsive design

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -14,7 +14,7 @@ const HomePage = () => {
         Recipe Sharing Platform
       </h1>
 
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-6 grid-cols-1 md: grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
         {recipes.map((recipe) => (
           <div
             key={recipe.id}
