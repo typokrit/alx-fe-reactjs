a52c4541f4e3a4c23a925683cda0e379050fd82e
typokrit <abenianemaw731@gmail.com>
2025-08-11 14:40:30 +0300

    Start Project

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./public/index.html"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
